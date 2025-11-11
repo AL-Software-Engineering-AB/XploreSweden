2924b529f82564425e505ae757e7b1c794339b7b
Linus Rengbrandt <line.rengbrandt@gmail.com>
2025-11-11 15:44:37 +0100

    Initial Commit

---
+++ XploreSweden/XploreSwedenApp.swift
@@ -0,0 +1,17 @@
+//
+//  XploreSwedenApp.swift
+//  XploreSweden
+//
+//  Created by Linus Rengbrandt on 2025-11-11.
+//
+
+import SwiftUI
+
+@main
+struct XploreSwedenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XploreSwedenTests/XploreSwedenTests.swift
@@ -0,0 +1,17 @@
+//
+//  XploreSwedenTests.swift
+//  XploreSwedenTests
+//
+//  Created by Linus Rengbrandt on 2025-11-11.
+//
+
+import Testing
+@testable import XploreSweden
+
+struct XploreSwedenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
