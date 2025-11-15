a44d87d4b8f3c9762fcac84d47eb4f3f973cd38e
Linus Rengbrandt <line.rengbrandt@gmail.com>
2025-11-15 15:04:04 +0100

    Fixed clustering on map pins to get rid of lag to not load all data simultaneously

---
+++ XploreSweden/ClusteredMapView.swift
@@ -0,0 +1,8 @@
+//
+//  ClusteredMapView.swift
+//  XploreSweden
+//
+//  Created by Linus Rengbrandt on 2025-11-15.
+//
+
+import Foundation
---
+++ XploreSweden/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  XploreSweden
+//
+//  Created by Linus Rengbrandt on 2025-11-15.
+//
+
+import Foundation
